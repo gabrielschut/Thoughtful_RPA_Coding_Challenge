aa42e8c2c636a8b6679b57bf8c0a89163027d5b1
Gabriel Schütt de Almeida <gabrielschuttdealmeida@gmail.com>
2024-08-07 18:53:04 -0300

    feat: finding and clicking on searshed topic

--- Scrapping/newsScraping.py
+++ Scrapping/newsScraping.py
@@ -11,14 +11,23 @@ options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service= service, options= options)
 try:
     driver.maximize_window()
-    driver.get("https://apnews.com/")
+    driver.get('https://www.latimes.com/')
     time.sleep(5)
-    driver.find_element(By.CLASS_NAME, 'SearchOverlay-search-button').click()
-    time.sleep(5)
-    searchbar = driver.find_element(By.CLASS_NAME, 'SearchOverlay-search-label')
-    searchbar.send_keys('Trump')
+    driver.execute_script('window.scrollBy(0, 900);')
+    time.sleep(3)
+    driver.find_element(By.CSS_SELECTOR, '[data-element="search-button"]').click()
+    time.sleep(3)
+    searchbar = driver.find_element(By.CSS_SELECTOR, '[data-element="search-form-input"]')
+    searchbar.send_keys('Gold')
     searchbar.send_keys(Keys.ENTER)
-    time.sleep(5)
+    time.sleep(3)
+    topics = driver.find_elements(By.CLASS_NAME, 'checkbox-input-label')
+    see_all = driver.find_elements(By.CSS_SELECTOR, '[data-toggle-trigger="see-all"]')
+    for i in topics:
+        if (i.text == topic):
+            i.click()
+            break
+    time.sleep(10)
 
 finally:
     driver.quit()
