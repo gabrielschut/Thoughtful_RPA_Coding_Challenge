f706f29ab32c75a828233b483ad4c4bb10f5479b
Gabriel Schütt de Almeida <gabrielschuttdealmeida@gmail.com>
2024-08-11 01:59:27 -0300

    feat: working robot

--- news_scrapper/xls_generator.py
+++ news_scrapper/xls_generator.py
@@ -2,9 +2,9 @@ import pandas as pd
 import news_scraping
 from datetime import datetime
 
-found_articles = news_scraping.la_news_scrapper('Brazil', '', 3)
-data_frame = pd.DataFrame([vars(article) for article in found_articles])
-data_frame.rename(columns={'publich_date':'date', 'picture_file_name':'picture filename',
-        'times_search_term_appears':'count of search phrases in the title and description'}, inplace= True)
-file_name = 'scrapped_articles_' + datetime.now().strftime("%m-%d-%Y %H:%M:%S").replace(" ", "_") + '.xlsx'
-data_frame.to_excel(file_name, index=False)
+def generate_xlsx_from_articles(articles_list):
+        data_frame = pd.DataFrame([vars(article) for article in articles_list])
+        data_frame.rename(columns={'publich_date':'date', 'picture_file_name':'picture filename',
+                'times_search_term_appears':'count of search phrases in the title and description'}, inplace= True)
+        file_name = 'scrapped_articles_' + datetime.now().strftime("%m-%d-%Y %H:%M:%S").replace(" ", "_") + '.xlsx'
+        data_frame.to_excel(file_name, index=False)
